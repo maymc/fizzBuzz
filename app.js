079b57250d03e9aa9b9d27d526a82a3ebc1583a2
May Chen <maychen@Mays-MacBook-Pro.local>
2018-04-25 00:09:17 -1000

    completed feature2 practice

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ if the number is a multiple of 3, it will console.log "fizz",
 if the number is a multiple of 5, it will console.log "buzz",  
 if the number is a multiple of 3 and 5, it will console.log "fizzBuzz"
 */
+
+for(var i=1; i<=100; i++){
+	console.log(i);
+	if(i%3 === 0)
+		console.log("fizz");
+	if(i%5 === 0)
+		console.log("buzz");
+	if(i%3 === 0 && i%5 === 0)
+		console.log("fizzBuzz");
+}
